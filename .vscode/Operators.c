ea4a204b35159c251a5a207a1b46089b01e6b099
rukminivilla <184979507+rukminivilla@users.noreply.github.com>
2025-08-26 20:10:48 +0530

    First commit - Added C program

---
+++ .vscode/Operators.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int a = 10, b = 5;   // integer variables
+    float x = 5.5, y = 2.2; // float variables
+
+    // Arithmetic operations
+    printf("Addition: %d\n", a + b);       // integer addition
+    printf("Subtraction: %d\n", a - b);    // integer subtraction
+    printf("Multiplication: %d\n", a * b); // integer multiplication
+    printf("Division: %d\n", a / b);       // integer division
+    printf("Float division: %.2f\n", x / y); // float division
+
+    return 0;
+}
+
+
